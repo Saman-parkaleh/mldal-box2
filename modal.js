1b0f38286c4b520bf1d84d4fc0b53ac8bc7e0799
saman <ssaman.parkaleh76@gmail.com>
2022-09-21 10:31:05 +0430

    first commit

---
+++ modal.js
@@ -0,0 +1,8 @@
+function doit(){
+  const modal = document.getElementById("box");
+  modal.classList.add("active");
+}
+function redo() {
+  const modal = document.getElementById("box");
+  modal.classList.remove("active");
+}
